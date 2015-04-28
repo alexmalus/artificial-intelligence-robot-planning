bb2fcf182f271deec3caa1c597871f37987acdfc
Joakim Hornstrup <s133326@student.dtu.dk>
2015-04-28 13:15:54 +0200

    minor changes to IOstreamServerComm, got error in project. Method not set properly up.

--- AI_Ateam/src/client/ateam/ServerComm/IOstreamServerComm.java
+++ AI_Ateam/src/client/ateam/ServerComm/IOstreamServerComm.java
@@ -1,11 +1,16 @@
 package client.ateam.ServerComm;
 
+import client.ateam.Level.Action;
+
+import java.util.ArrayList;
+
 /**
  * Created by Lasse on 25-04-2015.
  */
 public class IOstreamServerComm implements IServerComm {
+
     @Override
-    public void sendJointAction() {
+    public void sendJointAction(ArrayList<Action> actions) {
 
     }
 }
