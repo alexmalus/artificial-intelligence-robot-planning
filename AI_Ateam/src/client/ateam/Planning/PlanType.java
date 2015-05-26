8b53dd240114c898a399f302ba720212f2e0ddca
Lasse <lassebalmer@hotmail.com>
2015-05-26 21:03:41 +0200

    project clean up

---
+++ AI_Ateam/src/client/ateam/Planning/PlanType.java
@@ -0,0 +1,8 @@
+package client.ateam.Planning;
+
+/**
+ * Created by Lasse on 5/26/15.
+ */
+public enum PlanType {
+    FindBox, MoveBoxToGoal, Idle, AskForHelp, HelpOther
+}
--- AI_Ateam/src/client/ateam/conflictHandler/Resolution.java
+++
@@ -1,7 +0,0 @@
-package client.ateam.conflictHandler;
-
-/**
- * Created by joh on 21/04/15.
- */
-public class Resolution {
-}
